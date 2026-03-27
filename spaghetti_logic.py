b41960e150f62e2f28fe4e507cea2b148edd90be
EceeBasaran <ubasaran23@posta.pau.edu.tr>
2026-03-28 00:44:47 +0300

    Ic ice klasorler temizlendi ve dogru dosya guncellendi

--- spaghetti_logic.py
+++ spaghetti_logic.py
@@ -1,8 +1,43 @@
-def f(a, b):
-    x = a + b
-    y = a - b
-    z = x * y
-    if z > 0:
-        for i in range(z):
-            print("islem yapiliyor...")
-    return z
+def calculate_difference_product(first_number, second_number):
+    """
+    İki sayının toplamı ve farkının çarpımını hesaplar.
+    
+    Args:
+        first_number: İlk sayı
+        second_number: İkinci sayı
+    
+    Returns:
+        Toplam ve farkın çarpımı
+    """
+    sum_result = first_number + second_number
+    difference = first_number - second_number
+    return sum_result * difference
+
+
+def perform_operations_if_positive(value, operation_count):
+    """
+    Değer pozitifse belirtilen sayıda işlem gerçekleştirir.
+    
+    Args:
+        value: Kontrol edilecek değer
+        operation_count: Gerçekleştirilecek işlem sayısı
+    """
+    if value > 0:
+        for iteration in range(operation_count):
+            print("İşlem gerçekleştiriliyor...")
+
+
+def process_numbers(first_number, second_number):
+    """
+    İki sayı üzerinde hesaplama ve işlem gerçekleştirir.
+    
+    Args:
+        first_number: İlk sayı
+        second_number: İkinci sayı
+    
+    Returns:
+        Hesaplanan çarpım sonucu
+    """
+    product = calculate_difference_product(first_number, second_number)
+    perform_operations_if_positive(product, product)
+    return product
